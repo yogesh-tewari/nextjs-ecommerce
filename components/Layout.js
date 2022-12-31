c07e995bc3cb8ae90f82b6df3230981349785804
yogesh <yogeshtewari16@gmail.com>
2022-12-31 19:35:44 +0530

    fixed Link error by upgrading codemod new-link

--- components/Layout.js
+++ components/Layout.js
@@ -3,32 +3,30 @@ import Link from 'next/link';
 import React from 'react';
 
 export default function Layout({ title, children }) {
-  return (
-    <>
-      <Head>
-        <title>{title ? title : 'Being Wolves Store'}</title>
-        <meta name='description' content='Being Wolves Store ' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <div className='flex flex-col justify-between'>
-        <header>
-          <nav>
-            <Link href='/'>
-              <a className='logo text-xl font-bold'>Being Wolves</a>
+  return <>
+    <Head>
+      <title>{title ? title : 'Being Wolves Store'}</title>
+      <meta name='description' content='Being Wolves Store ' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+    <div className='flex flex-col justify-between'>
+      <header>
+        <nav>
+          <Link href='/' className='logo text-xl font-bold'>
+            Being Wolves
+          </Link>
+          <div>
+            <Link href='/cart' className='p-2'>
+              Cart
             </Link>
-            <div>
-              <Link href='/cart'>
-                <a className='p-2'>Cart</a>
-              </Link>
-              <Link href='/login'>
-                <a className='p-2'>Login</a>
-              </Link>
-            </div>
-          </nav>
-        </header>
-        <main>{children}</main>
-        <footer>©2022-23 Being Wolves. All Rights Reserved.</footer>
-      </div>
-    </>
-  );
+            <Link href='/login' className='p-2'>
+              Login
+            </Link>
+          </div>
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer>©2022-23 Being Wolves. All Rights Reserved.</footer>
+    </div>
+  </>;
 }
